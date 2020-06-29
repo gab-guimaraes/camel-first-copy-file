8e2a8cec004aba1d7c21630e5af15f4b1f442f98
Gabriel Guimaraes <dedito182@gmail.com>
2020-06-29 14:57:25 -0300

    first commit

---
+++ src/main/java/com/learncamel/file/CopyFileCamel.java
@@ -0,0 +1,32 @@
+package com.learncamel.file;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class CopyFileCamel {
+
+    public static void main(String [] args) throws Exception {
+        CamelContext context = new DefaultCamelContext();
+
+        try {
+            context.addRoutes(new RouteBuilder() {
+                @Override
+                public void configure() throws Exception {
+                    from("file:data/input?noop=true")
+                    .to("file:data/output");
+                }
+            });
+
+            context.start();
+
+            Thread.sleep(5000);
+
+            context.stop();
+        }catch (Exception e) {
+            System.out.println("Exception is " + e);
+            e.printStackTrace();
+
+        }}
+}
